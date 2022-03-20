e6e789ab9498be752d644e42ada7e3c7e46f1005
Abinaya2295 <101945749+Abinaya2295@users.noreply.github.com>
2022-03-20 21:29:42 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -4,6 +4,8 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Input 8
+		// Output 0 1 1 2 3 5 8 13 21 34
        int range = 8, firstNum = 0, secNum = 1, Sum;
        System.out.print(firstNum+" "+secNum);
        for(int i = 2;i < range; i++)
